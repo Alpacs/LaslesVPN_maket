9f34a40f5a1eeafdf7ee22fb359d513f40151eea
Dmitrij Kashlakov <78339355+Alpacs@users.noreply.github.com>
2021-11-21 21:28:35 +0400

    Add files via upload

---
+++ Java/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.reviews_block').slick({
+        dots: true,
+        arrows: true,
+        adaptiveHeight: true,
+        /*adaptiveWidth: true,*/
+        slidesToShow: 3,
+    });
+});
