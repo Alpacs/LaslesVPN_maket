bffaf747db49cc0f896c9f628562d923dec61187
Dmitrij Kashlakov <78339355+Alpacs@users.noreply.github.com>
2021-11-24 13:10:38 +0400

    Add files via upload

--- Java/script.js
+++ Java/script.js
@@ -5,5 +5,31 @@ $(document).ready(function() {
         adaptiveHeight: true,
         /*adaptiveWidth: true,*/
         slidesToShow: 3,
+        responsive: [{
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 2,
+                arrows: false
+            }
+        }]
+    });
+});
+$(document).ready(function() {
+    $('.header_burger').click(function(event) {
+        $('.header_burger').toggleClass('active');
+        $('.menu_option').toggleClass('menu_option_active');
+        $('.menu_option_button').toggleClass('menu_option_button_active');
+        $('.header').toggleClass('header_active');
+        $('#about_block').toggleClass('about_block_active');
+        $('body').toggleClass('lock'); //заблокировал скроллинг когда открыт бургер//
+
+        $('.menu_option_active a').on("click", function() {
+            $('.menu_option_active').removeClass('menu_option_active');
+            $('.active').removeClass('active');
+            $('.menu_option_button_active').removeClass('menu_option_button_active');
+            $('.about_block_active').removeClass('about_block_active');
+            $('.lock').removeClass('lock');
+            $('.header_active').removeClass('header_active');
+        });
     });
 });
